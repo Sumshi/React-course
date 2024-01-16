6e9299fa7e79e87e4f9d95b2d51638b4654246ba
Sumshi <sumayasomow@gmail.com>
2024-01-16 21:20:01 +0300

    ternary and && operators

---
+++ jsx-intro/jsx-conditionals.js
@@ -0,0 +1,49 @@
+// we cannot inject if statements onto jsx
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+let message;
+
+if (user.age >= drinkingAge) {
+  message = (
+    <h1>
+      Hey, check out this alcoholic beverage!
+    </h1>
+  );
+} else {
+  message = (
+    <h1>
+      Hey, check out these earrings I got at Claire's!
+    </h1>
+  );
+}
+
+root.render(message);
+
+
+// example 2
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === 'heads') {
+  img = <img src={pics.kitty} />
+} else {
+  img = <img src={pics.doggy} />
+}
+root.render(img);
