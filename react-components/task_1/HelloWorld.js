33753e7f316ec59ae173466ebd3b4e835779787e
Sumshi <sumayasomow@gmail.com>
2024-01-27 23:36:19 +0300

    done with components

---
+++ react-components/task_1/HelloWorld.js
@@ -0,0 +1,10 @@
+/*n the code editor, we’ve provided three files, App.js, index.js, and HelloWorld.js. Render a “Hello World!” greeting onto the screen with the appropriate file structure, where App.js contains the top-level component and index.js renders it.*/
+import React from 'react';
+
+function HelloWorld() {
+    return (
+      <h1>Hello World</h1>
+    );
+};
+
+export default HelloWorld;
---
+++ react-components/task_1/Index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HelloWorld from './App';
+ReactDOM.createRoot(document.getElementById('app')).render(<HelloWorld />);
